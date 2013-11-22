f50c15c12c3b0cfbea7d68f85a5e1121abd84205
Sérgio Vilar <sergiovilar.r@gmail.com>
2013-11-22 12:35:03 -0300

    Changes to pass in jshint

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var express = require('express');
+/*global require,console*/
 
-var app = express();
+(function () {
+    
+    "use strict";
+    
+    var express = require('express'),
+        exphbs = require('express3-handlebars'),
+        app = express();
 
-app.get('/', function (req, res) {
-    res.send('hey, this works!');
-});
+    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+    
+    app.get('/', function (req, res, next) {
+        res.send('<p>hey, this works!</p>');
+    });
+    
+    app.get('/photos', function (req, res) {
+        
+    });
+    
+    app.listen(3000, function () {
+        console.log('app serving on port 3000');
+    });
 
-app.listen(3000, function () {
-    console.log('app serving on port 3000');
-});
+    
+}());
